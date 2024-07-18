0bf7ae00c0952199c24d63a54cb7cb5f8a460535
Marvin Schirrmacher <marvin.schirrmacher@paymenttools.com>
2024-07-18 10:56:06 +0200

    fix tests due to new api spec flag

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -11,7 +11,7 @@ class TestParseArguments(unittest.TestCase):
         [
             "program_name",
             "-cve",
-            "tests/files/log4j.txt",
+            "tests/files/cve-advisories.json",
             "-arc",
             "tests/files/api-gateway.d2",
             "-dep",
@@ -22,6 +22,8 @@ class TestParseArguments(unittest.TestCase):
             "severity",
             "-of",
             "json",
+            "-api",
+            "tests/files/swagger-open-api.json",
         ],
     )
     def test_parse_arguments(self):
@@ -30,7 +32,7 @@ class TestParseArguments(unittest.TestCase):
 
         self.assertEqual(
             simplify_string(args.cve_description.read()),
-            "Apache Log4j2 2.0-beta9 throug",
+            '[    {        "name": "CVE-202',
         )
         self.assertEqual(
             simplify_string(args.architecture_diagram.read()),
@@ -39,6 +41,10 @@ class TestParseArguments(unittest.TestCase):
         self.assertEqual(
             simplify_string(args.dependency_list.read()),
             '{    "spdxVersion": "SPDX-2.3"',
+        )
+        self.assertEqual(
+            simplify_string(args.api_documentation.read()),
+            '{"components":{"schemas":{"Act',
         )
         self.assertEqual(args.large_language_model, "Mistral")
         self.assertEqual(args.output, "severity")
